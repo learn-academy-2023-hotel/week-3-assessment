18a911d7f6270a3d3464ae80a2a17182b0ed9706
Sarah Proctor <sproctor950@gmail.com>
2019-08-15 11:59:35 -0700

    build out for JS assessment 3

---
+++ js-intermediate.js
@@ -0,0 +1,105 @@
+// ASSESSMENT 3: INTERMEDIATE JAVASCRIPT
+// Coding practical questions
+
+// Be sure to use all given test cases
+
+// 1. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+
+// 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
+
+var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// Expected output: [9, 7, 199, -9]
+var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// Expected output: [7, 23, -823]
+
+
+
+// 3. Given the object below, complete the console.log to find specific information:
+
+var bicycle = {
+	type: "Roadbike",
+	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+	wheels: {
+		count: 2,
+		specs: ["road tires", "AX-7563", "80-115 PSI"],
+		brand: "Trek"
+	}
+}
+
+// Log the type of bicycle:
+console.log()
+
+// Log the bell:
+console.log()
+
+// Log the PSI:
+console.log()
+
+
+// 4. Write a function that takes one argument of type array and returns the contents of that array in reverse order.
+
+var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
+// Expected output: [7, 6, 5, 4, 3, 2, 1],
+var originalArray2 = ["a", "t", "l", "e", "d"]
+// Expected output: ["d", "e", "l", "t", "a"]
+
+
+
+// 5. Write the code that would make this test pass.
+
+describe("multTwo", () => {
+    test ("returns an array with all the numbers multiplied by two", () => {
+        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+    })
+})
+
+
+
+// 6. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
+
+var ourString = "Hello Learn Students!"
+// Expected output: 3
+
+
+
+// 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
+
+var middleLetters1 = "hello"
+// Expected output: “l”
+var middleLetters2 = "lamas"
+// Expected output: “am”
+
+
+
+
+// 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
+
+
+
+
+// 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
+// Don't overthink this one, it's just destrcturing practice.
+
+var myCar = {
+  make: "VW",
+  model: "Jetta",
+  specs: {
+    doors: 4,
+    transmission: "manual",
+  }
+}
+
+
+
+// 10. Stretch: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+var numbersToAdd1 = [2, 4, 45, 9]
+// Excpected output: [2, 6, 51, 60]
+var numbersToAdd2 = [0, 7, -8, 12]
+// Expected output: [0, 7, -1, 11]
+var numbersToAdd3 = []
+// Expected output: []
---
+++ js-intermediate.md
@@ -0,0 +1,98 @@
+# ASSESSMENT 3: INTERMEDIATE JAVASCRIPT
+## Interview Practice Questions
+
+Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn.
+
+1. What are the HTTP verbs? Which CRUD action corresponds to each verb?
+
+  Your answer:
+
+  Researched answer:
+
+
+2. Why would you use object destructuring?
+
+  Your answer:
+
+  Researched answer:
+
+
+
+3. Why is testing important?
+
+  Your answer:
+
+  Researched answer:
+
+
+4. What is the difference between a class and an object?
+
+  Your answer:
+
+  Researched answer:
+
+
+5. What did you learn during the group project this week? Please include any additional feedback you may have.
+
+  Your answer:
+
+
+
+### HTML/CSS Review questions: First, try to answer each question on your own then Google the answer to further your knowledge.
+
+1. How do you link a CSS file to your HTML page?
+
+  Your answer:
+
+  Researched answer:
+
+
+2. What is the difference between a div and a span?
+
+  Your answer:
+
+  Researched answer:
+
+
+3. What is a CSS class? When should you use an id instead of a class?
+
+  Your answer:
+
+  Researched answer:
+
+
+4. Name 4 semantic HTML tags.
+
+  Your answer:
+
+  Researched answer:
+
+
+5. What are three options for creating responsive design?
+
+  Your answer:
+
+  Researched answer:
+
+
+### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
+
+1. What is front end development? Can you identify any tools/skills that are uniquely required of front end developers?
+
+  Your answer:
+
+  Researched answer:
+
+
+2. What is block scope in JavaScript?
+
+  Your answer:
+
+  Researched answer:
+
+
+3. How would you explain the idea of "inheritance" in object oriented programming?
+
+  Your answer:
+
+  Researched answer:
